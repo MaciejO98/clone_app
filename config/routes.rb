d66c1bc2cb680e9c8fbf340dd8b84874badce4b5
Maciekgit config --global user.name Maciekgit config --global user.name Maciek <mopalskilowicz@gmail.com>
2021-05-11 11:55:56 +0200

    Pobieranie i wyświetlanie informacji o użytkowniku

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
         sessions: 'users/sessions',
         registrations: 'users/registrations'
       }
-
+  get 'users/:id' => 'users#show'
   root 'pages#index'
 end
